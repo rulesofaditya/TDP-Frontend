541ea097b884870bd018c3f16bbb55723c624a4e
rulesofaditya <ap7295@srmist.edu.in>
2023-07-03 15:26:53 +0100

    creating functions

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ hello = () =>{
 }
 
 hello();
+
+function double(a){
+    let b = a*2;
+     console.log(b);
+}
+
+
+double(2);
+
+
+// array and functions
+let output = []
+let count = 1;
+function fizzbuzz(){
+    output.push = count;
+    count++;
+    console.log(output)
+}
+
+fizzbuzz();
+fizzbuzz();
+
+
+// loops
+
+for(i = 0; i < 5; i++){
+
+     i = i + 1;
+    console.log(i);
+}
